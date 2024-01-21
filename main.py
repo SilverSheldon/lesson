27532d5431664623b32976a3db8db5f5c36447b2
Nil Mulyashov <nilmulyashov@gmail.com>
2024-01-21 17:03:23 +0300

    Отредактировал main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+# start program...
+
 def print_hi(name):
     print(f'Hi, {name}')
 
 
-def say_bye(name):
-    print(f"Bye-bye, {name}")
-
-
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    say_bye('PyCharm')
+    print_hi('Neil')
