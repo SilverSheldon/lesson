40eca7200196dcaf2e42e24016de4a77330a61c7
Nil Mulyashov <nilmulyashov@gmail.com>
2024-01-21 16:29:59 +0300

    Добавлена функция say_bye

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def print_hi(name):
     print(f'Hi, {name}')
 
+
+def say_bye(name):
+    print(f"Bye-bye, {name}")
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
+    say_bye('PyCharm')
